228b70072cabc1126ed4350b4883d7704a17a824
Baptiste POUPEAU <poupeaub@iut-acy.local>
2025-06-10 08:20:39 +0200

    DataAccess

--- SAE_PILOT/Model/Revendeur.cs
+++ SAE_PILOT/Model/Revendeur.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TD3_BindingBDPension.Model;
 
 namespace SAE_PILOT.Model
 {
@@ -11,12 +14,12 @@ namespace SAE_PILOT.Model
         private int numReveneur;
         private string raisonSociale;
         private string adresseRue;
-        private int adresseCP;
+        private string adresseCP;
         private string adresseVille;
 
         private Revendeur () { }
         public Revendeur(int numReveneur, string raisonSociale, 
-            string adresseRue, int adresseCP, string adresseVille)
+            string adresseRue, string adresseCP, string adresseVille)
         {
             this.NumReveneur = numReveneur;
             this.RaisonSociale = raisonSociale;
@@ -25,7 +28,7 @@ namespace SAE_PILOT.Model
             this.AdresseVille = adresseVille;
         }
         public Revendeur(string raisonSociale,
-            string adresseRue, int adresseCP, string adresseVille)
+            string adresseRue, string adresseCP, string adresseVille)
         {
             this.RaisonSociale = raisonSociale;
             this.AdresseRue = adresseRue;
@@ -72,7 +75,7 @@ namespace SAE_PILOT.Model
             }
         }
 
-        public int AdresseCP
+        public string AdresseCP
         {
             get
             {
@@ -96,6 +99,33 @@ namespace SAE_PILOT.Model
             {
                 this.adresseVille = value;
             }
+        }
+        /*
+         
+         
+Colonne	Type	NOT NULL	Défaut	Contraintes	Actions	Commentaire
+numrevendeur
+raisonsociale
+adresserue
+adressecp
+adresseville
+         
+         */
+        public List<Revendeur> FindAll()
+        {
+            List<Revendeur> lesRevendeurs = new List<Revendeur>();
+            using (NpgsqlCommand cmdSelect = new NpgsqlCommand("select * from revendeur;"))
+            {
+                DataTable dt = DataAccess.Instance.ExecuteSelect(cmdSelect);
+                foreach (DataRow dr in dt.Rows)
+                    lesRevendeurs.Add(new Revendeur((Int32)dr["numrevendeur"],
+                        (String)dr["raisonsociale"],
+                        (String)dr["adresserue"],
+                        (String)dr["adressecp"],
+                        (String)dr["adresserue"]
+                    ));
+            }
+            return lesRevendeurs;
         }
     }
 }
