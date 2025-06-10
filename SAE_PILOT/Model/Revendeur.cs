075a687aa76fe94ca72023042bc53859f2868081
Baptiste POUPEAU <poupeaub@iut-acy.local>
2025-06-10 11:43:15 +0200

    Revendeur requêtes SQL
    Ceci est une description owo

--- SAE_PILOT/Model/Revendeur.cs
+++ SAE_PILOT/Model/Revendeur.cs
@@ -9,7 +9,7 @@ using TD3_BindingBDPension.Model;
 
 namespace SAE_PILOT.Model
 {
-    public class Revendeur
+    public class Revendeur : ICrud<Revendeur>
     {
         private int numReveneur;
         private string raisonSociale;
@@ -126,6 +126,57 @@ adresseville
                     ));
             }
             return lesRevendeurs;
+        }
+
+        public int Create()
+        {
+            int nb = 0;
+            using (var cmdInsert = new NpgsqlCommand("INSERT INTO revendeur (raisonsociale,adresserue,adressecp,adresseville) values (@raisonsociale,@adresserue,@adressecp,@adresseville) RETURNING numrevendeur"))
+            {
+                cmdInsert.Parameters.AddWithValue("raisonsociale", this.RaisonSociale);
+                cmdInsert.Parameters.AddWithValue("adresserue", this.AdresseRue);
+                cmdInsert.Parameters.AddWithValue("adressecp", this.AdresseCP);
+                cmdInsert.Parameters.AddWithValue("adresseville", this.AdresseVille);
+                nb = DataAccess.Instance.ExecuteInsert(cmdInsert);
+            }
+            this.NumReveneur = nb;
+            return nb;
+        }
+
+        public void Read()
+        {
+            using (var cmdSelect = new NpgsqlCommand("SELECT * FROM revendeur WHERE numrevendeur = @numrevendeur"))
+            {
+                cmdSelect.Parameters.AddWithValue("numrevendeur", this.NumReveneur);
+                DataTable dt = DataAccess.Instance.ExecuteSelect(cmdSelect);
+                this.RaisonSociale = (String)dt.Rows[0]["raisonsociale"];
+                this.AdresseRue = (String)dt.Rows[0]["adresserue"];
+                this.AdresseCP = (String)dt.Rows[0]["adressecp"];
+                this.AdresseVille = (String)dt.Rows[0]["adresseville"];
+            }
+        }
+
+        public int Update()
+        {
+            using (var cmdUpdate = new NpgsqlCommand("UPDATE revendeur SET raisonsociale=@raisonsociale, adresserue=@adresserue, adressecp=@adressecp, adresseville=@adresseville WHERE numrevendeur=@numrevendeur"))
+            {
+                cmdUpdate.Parameters.AddWithValue("numrevendeur", this.NumReveneur);
+                cmdUpdate.Parameters.AddWithValue("raisonsociale", this.RaisonSociale);
+                cmdUpdate.Parameters.AddWithValue("adresserue", this.AdresseRue);
+                cmdUpdate.Parameters.AddWithValue("adressecp", this.AdresseCP);
+                cmdUpdate.Parameters.AddWithValue("adresseville", this.AdresseVille);
+                return DataAccess.Instance.ExecuteSet(cmdUpdate);
+            }
+        }
+
+        public int Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Revendeur> FindBySelection(string criteres)
+        {
+            throw new NotImplementedException();
         }
     }
 }
