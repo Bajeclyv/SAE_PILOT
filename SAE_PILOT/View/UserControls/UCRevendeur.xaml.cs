7d9d8a43d10e3f87cba988f1984d3dbebf1d105c
Yoenie DUPUY <dupuyy@iut-acy.local>
2025-06-11 17:22:59 +0200

    Rechercher revendeur (3 filtres)

--- SAE_PILOT/View/UserControls/UCRevendeur.xaml.cs
+++ SAE_PILOT/View/UserControls/UCRevendeur.xaml.cs
@@ -27,16 +27,36 @@ namespace SAE_PILOT.View.UserControls
         {
             InitializeComponent();
             this.DataContext = new GestionRevendeur();
-            //dgRevendeur.Items.Filter = RechercherSocialeRevendeur;
+            dgRevendeur.Items.Filter = RechercherSocialeRevendeur;
         }
 
-        /*private bool RechercherSocialeRevendeur(object obj)
+        private bool RechercherSocialeRevendeur(object obj)
         {
-            if (String.IsNullOrEmpty(txtSociale.Text &&))
+            if (String.IsNullOrEmpty(txtSociale.Text) && String.IsNullOrEmpty(txtCPRevendeur.Text) && String.IsNullOrEmpty(txtVilleRevendeur.Text))
                 return true;
-            Chien unChien = obj as Chien;
-            return (unChien.Nom.StartsWith(textMotClefChien.Text, StringComparison.OrdinalIgnoreCase) || unChien.Maitre.StartsWith(textMotClefChien.Text, StringComparison.OrdinalIgnoreCase));
-        }*/
+
+            bool filtreSociale = true;
+            bool filtreCP = true;
+            bool filtreVille = true;
+
+            Revendeur unRevendeur = obj as Revendeur;
+        
+            if (!String.IsNullOrEmpty(txtSociale.Text))
+                filtreSociale = unRevendeur.RaisonSociale.StartsWith(txtSociale.Text, StringComparison.OrdinalIgnoreCase);
+
+            if (!String.IsNullOrEmpty(txtCPRevendeur.Text))
+                filtreCP = unRevendeur.AdresseCP.ToLower().StartsWith(txtCPRevendeur.Text, StringComparison.OrdinalIgnoreCase);
+
+            if (!String.IsNullOrEmpty(txtVilleRevendeur.Text))
+                filtreVille = unRevendeur.AdresseVille.ToLower().StartsWith(txtVilleRevendeur.Text, StringComparison.OrdinalIgnoreCase);
+
+            return filtreSociale && filtreCP && filtreVille;
+        }
+        private void txtRevendeur_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CollectionViewSource.GetDefaultView(dgRevendeur.ItemsSource).Refresh();
+
+        }
 
         private void butCreerRevendeur_Click(object sender, RoutedEventArgs e)
         {
