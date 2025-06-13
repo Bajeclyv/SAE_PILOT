d13a087150cd4b247ea43e188d892fae1bedceea
Baptiste Poupeau <Baptiste.Poupeau@etu.univ-smb.fr>
2025-06-13 11:39:52 +0200

    Tests unitaires (à terminer)

---
+++ SAE_PILOTTests/Model/CommandeTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SAE_PILOT.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAE_PILOT.Model.Tests
+{
+    [TestClass()]
+    public class CommandeTests
+    {
+        [TestMethod()]
+        public void CommandeTest_Employe()
+        {
+            Commande commande = new Commande(1, 1, 1, 1, DateTime.Today, DateTime.Today);
+            Assert.AreEqual("Poupeau Baptiste", commande.NomCompletEmploye, "Nom incorrect");
+        }
+        [TestMethod()]
+        public void CommandeTest_Transport()
+        {
+            Commande commande = new Commande(1, 1, 1, 1, DateTime.Today, DateTime.Today);
+            Assert.AreEqual("UPS", commande.NomTransport, "Nom incorrect");
+        }
+    }
+}
