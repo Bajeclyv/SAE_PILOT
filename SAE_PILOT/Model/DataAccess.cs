54187e7dfa754577b8299662dc33dec611bad883
yo <Yoenie.Dupuy@etu.univ-smb.fr>
2025-06-15 18:23:30 +0200

    Dernier commit pour DataAccess a l'IUT

--- SAE_PILOT/Model/DataAccess.cs
+++ SAE_PILOT/Model/DataAccess.cs
@@ -13,8 +13,10 @@ namespace TD3_BindingBDPension.Model
     public  class DataAccess
     {
         private static readonly DataAccess instance = new DataAccess();
-        //private readonly string connectionString = "Host=srv-peda-new;Port=5433;Username=poupeaub;Password=62TVka;Database=poupeaub_sae201;Options='-c search_path=poupeaub'";
-        private readonly string connectionString = "Host=srv-peda.iut-acy.local;Port=5433;Username=poupeaub;Password=62TVka;Database=poupeaub_sae201;Options='-c search_path=poupeaub'";
+        private readonly string connectionString = "Host=srv-peda-new;Port=5433;Username=poupeaub;Password=62TVka;Database=poupeaub_sae201;Options='-c search_path=poupeaub'";
+        //private readonly string connectionString = "Host=srv-peda.iut-acy.local;Port=5433;Username=poupeaub;Password=62TVka;Database=poupeaub_sae201;Options='-c search_path=poupeaub'";
+        //private readonly string connectionString = "Host=localhost;Port=5432;Username=postgres;Password=1201;Database=dupuyy_pilot;Options='-c search_path=public'";
+
         private NpgsqlConnection connection;
 
         public static DataAccess Instance
